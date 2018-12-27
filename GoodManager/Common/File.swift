cfa4eb80f58eff0a8c9abb447ae90d8d1456b73c
kevin <0321zyw@sina.com>
2018-12-27 14:28:25 +0800

    FileManager

---
+++ GoodManager/Common/File.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  GoodManager
+//
+//  Created by KevinZhao on 2018/12/27.
+//  Copyright © 2018 GoodManager. All rights reserved.
+//
+
+import Foundation
+
+let fileManager = FileManager.default
+
+func APPIfExistFile(path:String) -> Bool {
+    let isExists = fileManager.fileExists(atPath: NSHomeDirectory() + path)
+    return isExists
+}
+
+func APPDelFile(path:String) -> Bool{
+    do {
+        try fileManager.removeItem(atPath: NSHomeDirectory()+path)
+    }
+      catch {
+            print(error)
+            return false
+    }
+    return true
+}
+
+func APPGetFileSize(path:String) -> Int{
+    let filePath = NSHomeDirectory() + path
+    return Int(filePath.getFileSize())
+}
---
+++ GoodManager/Extension/ExString.swift
@@ -0,0 +1,46 @@
+//
+//  ExString.swift
+//  GoodManager
+//
+//  Created by KevinZhao on 2018/12/27.
+//  Copyright © 2018 GoodManager. All rights reserved.
+//
+
+import Foundation
+extension String {
+    // 对象方法
+    func getFileSize() -> UInt64  {
+        var size: UInt64 = 0
+        let fileManager = FileManager.default
+        var isDir: ObjCBool = false
+        let isExists = fileManager.fileExists(atPath: self, isDirectory: &isDir)
+        // 判断文件存在
+        if isExists {
+            // 是否为文件夹
+            if isDir.boolValue {
+                // 迭代器 存放文件夹下的所有文件名
+                let enumerator = fileManager.enumerator(atPath: self)
+                for subPath in enumerator! {
+                    // 获得全路径
+                    let fullPath = self.appending("/\(subPath)")
+                    do {
+                        let attr = try fileManager.attributesOfItem(atPath: fullPath)
+                        size += attr[FileAttributeKey.size] as! UInt64
+                    } catch  {
+                        print("error :\(error)")
+                    }
+                }
+            } else {    // 单文件
+                do {
+                    let attr = try fileManager.attributesOfItem(atPath: self)
+                    size += attr[FileAttributeKey.size] as! UInt64
+                    
+                } catch  {
+                    print("error :\(error)")
+                }
+            }
+        }
+        return size
+    }
+}
+
