a428fe162c6bc19f9c8caeee08a6b19602dd47f5
kevin <0321zyw@sina.com>
2019-02-26 17:36:44 +0800

    file download

--- GoodManager/Common/File.swift
+++ GoodManager/Common/File.swift
@@ -124,16 +124,16 @@ func APPDownFile(path:String, callBackfunName:String){
     //指定下载路径和保存文件名
     //指定下载路径（文件名不变）
     let destination: DownloadRequest.DownloadFileDestination = { _, response in
-        let documentsURL = URL(string: NSHomeDirectory() + "/Documents/localDocuments/")
-        let fileURL = documentsURL?.appendingPathComponent(response.suggestedFilename!)
+        let documentsURL = URL(fileURLWithPath: NSHomeDirectory() + "/Documents/localDocuments/")
+        let fileURL = documentsURL.appendingPathComponent(response.suggestedFilename!)
         //两个参数表示如果有同名文件则会覆盖，如果路径中文件夹不存在则会自动创建
-        return (fileURL!, [.removePreviousFile, .createIntermediateDirectories])
+        return (fileURL, [.removePreviousFile, .createIntermediateDirectories])
     }
     
     //需要下载的文件
-    let fileURL = URL(string: path)!
+    let fileURL = URL(string: path)
     //开始下载
-    Alamofire.download(fileURL, to: destination)
+    Alamofire.download(fileURL!, to: destination)
         .response { response in
             print("Download:")
             print(response.error)
