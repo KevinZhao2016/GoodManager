05c8defcc3a70b197516c8fe3542ccef35db786b
hao <1549057151@qq.com>
2019-03-18 07:42:20 +0800

    下载微博sdk

--- Pods/Target Support Files/Pods-GoodManager/Pods-GoodManager-resources.sh
+++ Pods/Target Support Files/Pods-GoodManager/Pods-GoodManager-resources.sh
@@ -98,9 +98,11 @@ EOM
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "${PODS_ROOT}/AliPay/AlipaySDK.bundle"
+  install_resource "${PODS_ROOT}/Weibo_SDK/libWeiboSDK/WeiboSDK.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "${PODS_ROOT}/AliPay/AlipaySDK.bundle"
+  install_resource "${PODS_ROOT}/Weibo_SDK/libWeiboSDK/WeiboSDK.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
