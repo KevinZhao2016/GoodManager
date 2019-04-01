790c103aba8a672c3abb3e5c2df37ca4086a3613
Liberate99 <1186400034@qq.com>
2019-04-01 22:47:32 +0800

    修改bug  文件上传crash

--- GoodManager/Common/File.swift
+++ GoodManager/Common/File.swift
@@ -55,16 +55,16 @@ func APPDelFile(path:String,callBackfunName:String){
 
 func APPGetFileSize(path:String) -> String{
     print("-------------APPGetFileSize--------------")
-    //    let fileType = _path.components(separatedBy: ".")[1]
+    // let fileType = _path.components(separatedBy: ".")[1]
     var _path = path
-    //路径整理
+    // 路径整理
     if (_path != "")&&(_path.contains("[\"")&&(_path.contains("\"]")) ) {
         _path = _path.components(separatedBy: "[\"")[1].components(separatedBy: "\"]")[0]
     }
     while (_path.contains(",")) {
         _path.remove(at: _path.firstIndex(of: ",")!)
     }
-    //两条路径
+    // 两条路径
     var TwoPath = [String]()
     if (_path.firstIndex(of: ".") != _path.lastIndex(of: ".")) {
         TwoPath = _path.components(separatedBy: ".")
@@ -72,27 +72,33 @@ func APPGetFileSize(path:String) -> String{
         TwoPath[1].append("."+TwoPath[2])
         var str = TwoPath[1].dropFirst(3)
         TwoPath[1] = String(str)
-        
+
         print("filePath_1:  " + TwoPath[0])
         print("filePath_2:  " + TwoPath[1])
     }
     let manager = FileManager.default
     let attributes = try? manager.attributesOfItem(atPath: _path) //结果为Dictionary类型
     print("filePath:  \(_path)")
-    print("fileSize:  \(attributes![FileAttributeKey.size]!)")
-    return "\(attributes![FileAttributeKey.size]!)"
+    if attributes != nil {
+        print("fileSize:  \(attributes![FileAttributeKey.size]!)")
+        return "\(attributes![FileAttributeKey.size]!)"
+    }else{
+        return "APPGetFileSize  --  error!: no such file!"
+    }
 }
 
 func APPGetFileBase(path:String) -> String{
+    print("-------------APPGetFileBase--------------")
     let fileUrl = URL(fileURLWithPath: path)
     let fileData = try? Data(contentsOf: fileUrl)
-    //将文件转为base64编码
+    // 将文件转为base64编码
     let base64 = fileData?.base64EncodedString(options: .endLineWithLineFeed)
     return base64 ?? ""
 }
 
 //在线预览文件
 func APPPreviewFile(path:String){
+    print("-------------APPPreviewFile--------------")
     let vc = getLastMainViewController()
     let filevc = PreviewFileViewController(Path: path)
 //    let controller = BaseNavigationController(rootViewController:PreviewFileViewController(Path: path))
@@ -100,7 +106,9 @@ func APPPreviewFile(path:String){
 }
 
 func APPUploadFile(path:String, domainName:String, folderName:String, callBackfunName:String){
+    print("-------------APPUploadFile--------------")
 //    let fileURL = URL(string: NSHomeDirectory() + "/Documents" + path)
+    let vc = getLastMainViewController()
     let LaunchProvider = MoyaProvider<LaunchTarget>()
     //TODO: 上传地址初始化
 //    Alamofire.upload(fileURL!, to: "host")
@@ -111,35 +119,46 @@ func APPUploadFile(path:String, domainName:String, folderName:String, callBackfu
     LaunchProvider.request(.uploadFile(path,domainName,folderName), completion: { result in
         switch result {
         case let .success(moyaResponse):
-            let data = moyaResponse.data // Data, your JSON response is probably in here!
-            let statusCode = moyaResponse.statusCode // Int - 200, 401, 500, etc
-            print(statusCode)
+            // Data, your JSON response is probably in here!
+            let data = moyaResponse.data
+            // Int - 200, 401, 500, etc
+            let statusCode = moyaResponse.statusCode
+            print("APPUploadFile  -- success: " + "\(statusCode)")
             if(statusCode == 200){
                 do {
                     if let model = try? moyaResponse.mapObject(LaunchResultModel.self) {
                         if(model.code == 1){
                             print(model)
                             //TODO：执行回调
-                            ExecWinJS(JSFun: callBackfunName + "(\"" + String(data: data, encoding: String.Encoding.utf8)! + "\")")
+                            ExecWinJS(JSFun: callBackfunName + "(" + String(data: data, encoding: String.Encoding.utf8)! + ")")
                         }
                     } else {
-                        print("maperror")
+                        print("map  error")
+                        let alert = UIAlertController(title: "文件上传失败！", message: "map error!", preferredStyle: UIAlertController.Style.alert)
+                        alert.addAction(UIAlertAction(title: "确认", style: UIAlertAction.Style.default, handler: nil))
+                        vc.present(alert, animated: true, completion: nil)
                     }
                 }catch {
-                    print(error)
+                    print("APPUploadFile  -- error:  "+"\(error)")
+                    let alert = UIAlertController(title: "文件上传失败！", message: "\(error)", preferredStyle: UIAlertController.Style.alert)
+                    alert.addAction(UIAlertAction(title: "确认", style: UIAlertAction.Style.default, handler: nil))
+                    vc.present(alert, animated: true, completion: nil)
                 }
             }
         case let .failure(error):
             guard let error = error as? CustomStringConvertible else {
                 break
             }
-            print(error)
+            let alert = UIAlertController(title: "文件上传失败！", message: "文件不存在", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "确认", style: UIAlertAction.Style.default, handler: nil))
+            vc.present(alert, animated: true, completion: nil)
+            print("APPUploadFile  -- error:  " + "\(error)")
         }
     })
-    
 }
 
 func APPDownFile(path:String, callBackfunName:String){
+    print("-------------APPDownFile--------------")
     //指定下载路径和保存文件名
     //指定下载路径（文件名不变）
     let destination: DownloadRequest.DownloadFileDestination = { _, response in
