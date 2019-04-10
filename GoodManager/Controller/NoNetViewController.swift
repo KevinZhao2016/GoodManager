4e823750def896ab4e78af24e67fac57bfa0a28b
Liberate99 <1186400034@qq.com>
2019-04-10 20:43:18 +0800

    修改无网络页

--- GoodManager/Controller/NoNetViewController.swift
+++ GoodManager/Controller/NoNetViewController.swift
@@ -15,40 +15,45 @@ class NoNetViewController: UIViewController {
     var SCWIDTH = UIScreen.main.bounds.width
     var SCHEIGHT = UIScreen.main.bounds.height
     
-    var label:UILabel = UILabel()
     
+    var imageView:UIImageView = UIImageView()
+    var label:UILabel = UILabel()
     var newButton:UIButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        label = UILabel(frame: CGRect(x: SCWIDTH/2-100, y: (SCHEIGHT/5)*1, width: 200, height: 90))
+        let image = UIImage(named: "pic_refresh_1")
+        imageView.image = image
+        //指定图片显示的位置
+//        imageView.frame.origin = CGPoint(x:UIScreen.main.bounds.width/2,y:100);
+        imageView.frame = CGRect(x:UIScreen.main.bounds.width/2-60,y:150,width:120,height:120)
+        self.view.addSubview(imageView)
+        
+        label = UILabel(frame: CGRect(x: SCWIDTH/2-100, y: 270, width: 200, height: 90))
         label.textColor = .darkGray
-        label.text = "请检查网络！"
+        label.text = "暂无网络，点击重试"
         label.textAlignment = .center
-//        tipsLabel.textAlignment = NSTextAlignmentCenter;
         self.view.addSubview(label)
         
-        newButton = UIButton(frame: CGRect(x: SCWIDTH/2-50, y: (SCHEIGHT/5)*1+100, width: 100, height: 40))
+        newButton = UIButton(frame: CGRect(x: SCWIDTH/2-50, y: 350, width: 100, height: 40))
         //开启遮罩（不开启遮罩设置圆角无效）
         newButton.layer.masksToBounds = true
         //设置圆角半径
         newButton.layer.cornerRadius = 5
         //设置按钮边框宽度
-        newButton.layer.borderWidth = 1
-        //设置按钮层边框颜色为浅灰色
-        newButton.layer.borderColor = UIColor.lightGray.cgColor
+        newButton.layer.borderWidth = 3
+        //设置按钮边框颜色
+        newButton.layer.borderColor = UIColor.init(red: 77/255, green: 191/255, blue: 255/255, alpha: 1).cgColor
         newButton.backgroundColor = .white
-        newButton.setTitle("刷新", for: .normal)
-        newButton.tintColor = .lightGray
-        newButton.setTitleColor(UIColor.lightGray, for: .normal)
+        newButton.setTitle("点击重试", for: .normal)
+        newButton.tintColor = .init(red: 77, green: 191, blue: 255, alpha: 1)
+        newButton.setTitleColor(UIColor.init(red: 77/255, green: 191/255, blue: 255/255, alpha: 1), for: .normal)
         newButton.setTitleColor(UIColor.darkGray, for: .highlighted)
         newButton.addTarget(self, action: #selector(newButtonAction), for: .touchUpInside)
         self.view.addSubview(newButton)
         
         self.view.backgroundColor = .white
-        
-        // Do any additional setup after loading the view.
     }
 
     @objc func newButtonAction() {
